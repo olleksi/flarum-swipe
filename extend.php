faa77d91691917afc9491bdc89dcc7e690951aed
olleksi <lieks.otaro@gmail.com>
2025-11-12 23:35:28 +0200

    Update extend.php

--- extend.php
+++ extend.php
@@ -1,47 +1,23 @@
 <?php
 
-namespace olleksi\FlarumSwipe;
-
 use Flarum\Extend;
-use Flarum\Frontend\Document;
 
 return [
     (new Extend\Frontend('forum'))
-        ->js(__DIR__ . '/js/dist/forum.js')
-        ->content(function (Document $document) {
-            $document->head[] = '<style>
-                .swipe-indicator {
-                    position: absolute;
-                    top: 6px;
-                    left: 50%;
-                    transform: translateX(-50%);
-                    width: 40px;
-                    height: 4px;
-                    border-radius: 2px;
-                    z-index: 1001;
-                    cursor: grab;
-                }
-                
-                @media (max-width: 768px) {
-                    .swipe-indicator {
-                        background-color: var(--primary-color) !important;
-                    }
-                    
-                    @media (prefers-color-scheme: dark) {
-                        .swipe-indicator {
-                            background-color: var(--primary-color) !important;
-                        }
-                    }
-                }
-                
-                .swipe-indicator:active {
-                    cursor: grabbing;
-                }
-                
-                .Dropdown-menu {
-                    padding-top: 20px !important;
-                }
-            </style>';
+        ->content(function () {
+            $js = @file_get_contents(__DIR__ . '/resources/swipe.js');
+            $css = @file_get_contents(__DIR__ . '/resources/swipe.css');
+            
+            $output = '';
+            
+            if ($js !== false) {
+                $output .= '<script type="text/javascript">' . $js . '</script>';
+            }
+            
+            if ($css !== false) {
+                $output .= '<style type="text/css">' . $css . '</style>';
+            }
+            
+            return $output;
         }),
-        
 ];
